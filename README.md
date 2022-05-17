d6ab00b600605c89eb6895e97eeb11ca5c8b3275
Aaron Stokes <stokescomp01@gmail.com>
2022-05-17 15:20:06 -0600

    add screenshot images to assets, add css color coding to uv index, complete readme and review

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# weatherdashboard
+# Your Weather Dashboard!/ weatherdashboard
+
+## Table of contents
+
+> - [Title / Repository Name](#title--repository-name)
+>   - [Table of contents](#table-of-contents)
+>   - [About / Synopsis](#about--synopsis)
+>   - [Features](#features)
+>   - [Usage](#usage)
+>   - [Technology](#technology)
+>   - [Credits](#credits)
+>   - [Contributing / Reporting issues](#contributing--reporting-issues)
+
+## About / Synopsis
+
+- This app offers a simple tool to retrieve both the current weather, and a 5-day forecast for the city of your choosing. The app is designed to provide a clean user interface for city searches, and the return of clear and understandable search results.
+- Your Weather Dashboard! was built as a bootcamp challenge.
+- If you would like to use a live deployed version of Your Weather Dashboard, pleae click [here.]()
+
+## Features
+
+    - Current weather forecast includes:
+        - Temperature in °F
+        - Humidity by %
+        - Wind Speed in MPH
+        - UV Index with color-coding by level
+    - Five Day Forecast with weather icons, temperature, and humidity
+    - Continually displays recent city searches
+
+### Usage
+
+![Your Weather Dashboard read to use](assets/images/weatherdashboardready.png?raw=true "Weather-Dashboard")
+![City Search (Denver) Using Your Weather Dashboard](assets/images/weatherdashboardcitysearch.png?raw=true "Weather-Dashboard-Denver")
+![Weather Dashboard city search resutl](?raw=true)
+![See your cities you have already searched (Denver/Sanfranciso)](assets/images/weatherdashboardsavedcity.png?raw=true "Weather-Dashboard-Saved-City")
+
+## Technology
+
+Your Weather Dashboard! was created utilizing the following:
+
+- [Visual Studio Code](https://code.visualstudio.com/)
+- [Open Weather API](https://openweathermap.org/)
+- [JavaScript](https://www.javascript.com/)
+- [Bootstrap](https://getbootstrap.com/)
+- [jQuery](https://jquery.com/)
+
+## Credits
+
+- [Bootstrap Help](https://www.creative-tim.com/blog/web-design/add-bootstrap-html-guide/)
+- [Beautiful Colors](https://flatuicolors.com/)
+- [Favicon Help](https://www.w3schools.com/html/html_favicon.asp)
+- [Favicon Image courtesy of](https://freepngimg.com/png/76810-forecasting-weather-forecast-icon-free-frame)
+- [README guide](https://www.freecodecamp.org/news/how-to-write-a-good-readme-file/)
+- [Musical inspiration]()
+
+## Contributing / Reporting issues
+
+- Your Weather Dashboard! was created as a [boot camp](https://bootcamp.berkeley.edu/coding/) project. If you are interested in contributing, or have questions about this project, please contact me [here.](mailto:stokescomp01@gmail.com)
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,6 @@ var weatherApiKey = "349f59965f00cd94141bf5e009d70cd5";
 
 function dayFormat(date) {
   var date = new Date();
-  console.log(date);
   var month = date.getMonth() + 1;
   var day = date.getDate();
 
@@ -33,7 +32,6 @@ function startApp() {
 
 function saveCities() {
   localStorage.setItem("cities", JSON.stringify(cities));
-  console.log(localStorage);
 }
 
 //next enter Function renturnCities()
@@ -48,7 +46,6 @@ function returnCities() {
     listEl.attr("id", "listCi");
     listEl.attr("data-city", city);
     listEl.attr("class", "list-group-item");
-    console.log(listEl);
     availableCities.prepend(listEl);
   }
   if (!city) {
@@ -116,17 +113,16 @@ function getWeatherResponse(cityName) {
       var cityUVp = $("<p>").text("UV Index: ");
       cityUVp.append(cityUV);
       $("#today-weather").append(cityUVp);
-      console.log(typeof responseuv.value);
       if (responseuv.value > 0 && responseuv.value <= 2) {
-        cityUV.attr("class", "green");
+        cityUV.attr("class", "juneBud");
       } else if (responseuv.value > 2 && responseuv.value <= 5) {
-        cityUV.attr("class", "yellow");
+        cityUV.attr("class", "lightGreenishBlue");
       } else if (responseuv.value > 5 && responseuv.value <= 7) {
-        cityUV.attr("class", "orange");
+        cityUV.attr("class", "shyMoment");
       } else if (responseuv.value > 7 && responseuv.value <= 10) {
-        cityUV.attr("class", "red");
+        cityUV.attr("class", "turbo");
       } else {
-        cityUV.attr("class", "purple");
+        cityUV.attr("class", "pinkGlamour");
       }
     });
 
@@ -141,7 +137,6 @@ function getWeatherResponse(cityName) {
       method: "GET",
     }).then(function (response5day) {
       $("#boxes").empty();
-      console.log(response5day);
       for (var i = 0, j = 0; j <= 5; i = i + 6) {
         var read_date = response5day.list[i].dt;
         if (response5day.list[i].dt != response5day.list[i + 1].dt) {
@@ -150,7 +145,6 @@ function getWeatherResponse(cityName) {
           var d = new Date(0);
           d.setUTCSeconds(read_date);
           var date = d;
-          console.log(date);
           var month = date.getMonth() + 1;
           var day = date.getDate();
           var dayOutput =
@@ -183,7 +177,6 @@ function getWeatherResponse(cityName) {
           }
 
           var pElTemperatureK = response5day.list[i].main.temp;
-          console.log(skyconditions);
           var temperatureAsNumber = parseInt((pElTemperatureK * 9) / 5 - 459);
           var pElTemperature = $("<p>").text(
             "Temperature: " + temperatureAsNumber + " °F"
@@ -196,7 +189,6 @@ function getWeatherResponse(cityName) {
           FivedayDiv.append(pElTemperature);
           FivedayDiv.append(pElHumidity);
           $("#boxes").append(FivedayDiv);
-          console.log(response5day);
           j++;
         }
       }
